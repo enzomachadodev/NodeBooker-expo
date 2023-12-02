b83993060bab4345d66a64001adcef5db674888a
m4chado <enzo653fm@gmail.com>
2023-12-02 19:20:02 -0300

    chore: add prettier and eslint setup

---
+++ .prettierrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  trailingComma: "es5",
+  tabWidth: 2,
+  semi: true,
+  singleQuote: false,
+  endOfLine: "lf",
+  overrides: [
+    {
+      files: ["*.js", "*.jsx", "*.ts", "*.tsx"],
+      options: {
+        parser: "typescript",
+      },
+    },
+  ],
+  ignorePatterns: ["**/*.svg"],
+};
